b904f08bca76b78dae0fb4c8fc0b50debe1afe63
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-04-26 08:46:17 +0200

    transfrom from hbs to ejs

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const pug = require('pug')
-const expressHbs = require('express-handlebars');
 
 const rootDir  = require('./utils/path')
 const adminData = require('./routes/admin')
@@ -11,16 +9,8 @@ const shopRoutes = require('./routes/shop')
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-const hbs = expressHbs.create({
-    layoutsDir: __dirname + '/views/layouts',
-    extname: 'hbs',
-    defaultLayout: 'main-layout', 
-    // defaultLayout: 'index_mirror',
-    // partialsDir: __dirname + '/views/partials',
-});
-
-app.set('view engine', 'hbs'); // set the view engine to desired templating engine
-app.engine('hbs', hbs.engine)
+app.set('view engine', 'ejs'); 
+// app.engine('hbs', hbs.engine)
 app.set('views', 'views'); // set the views directory
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -33,7 +23,7 @@ app.use('/', (req, res, next) => {
     console.log('Page not found middleware');
     // chaining fns => send has to be the last one
     // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
-    res.status(404).render('404', { pageTitle: 'Page not found HANDLEBARS'});
+    res.status(404).render('404', { pageTitle: 'Page not found EJS'});
 });
 
 app.listen(PORT, () => console.log('Server is runnint at port ', + PORT));
