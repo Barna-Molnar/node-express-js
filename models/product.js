28e821c9bfcfea0b270badb9d5e2689b1d797185
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-04-27 10:02:48 +0200

    refactor

--- models/product.js
+++ models/product.js
@@ -2,6 +2,16 @@ const path = require('path');
 const fs = require('fs');
 const rootDir = require('../utils/path');
 
+const getProductsFromFile = (cb) => {
+    const p = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+};
+
 module.exports = class Product {
     constructor({ title, price, description }) {
         this.title = title;
@@ -9,28 +19,15 @@ module.exports = class Product {
         this.description = description;
     }
     save() {
-        const p = path.join(rootDir, 'data', 'products.json');
-
-        fs.readFile(p, (err, fileContent) => {
-            // if file not found, create an empty array
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log({ err });
             });
-
         });
     }
 
     static fetchAll(cb) {
-        const p = path.join(rootDir, 'data', 'products.json');
-        return fs.readFile(p, (err, fileContent) => {
-            // console.log({ err });
-            // console.log(JSON.parse(fileContent));
-            return !err ? cb(JSON.parse(fileContent)) : cb([]);
-        });
+        getProductsFromFile(cb);
     }
 };
