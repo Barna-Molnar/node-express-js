a194280e24abe797cf852a45053ee183e8b8b068
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-04-29 08:52:51 +0200

    post Cart route

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart')
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
@@ -29,6 +30,14 @@ exports.getIndex = (req, res, next) => {
     });
 };
 
+exports.postCart = (req, res, next) => {
+    const productId = req.body.productId;
+    Product.findById(productId, (product) => {
+        Cart.addProduct(productId, product.price);
+    })
+    res.redirect('/cart');
+
+};
 exports.getCart = (req, res, next) => {
 
     res.render('shop/cart', {
