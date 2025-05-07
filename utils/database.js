894db6011a68308713cabbccfb0dcbff625bbf58
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-07 07:42:20 +0200

    set up sequelize

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,9 @@
-const mysql = require('mysql2/promise');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Bazdmeg1990',
-    database: 'node-server',
+
+const sequelize = new Sequelize('node-server', 'root', 'Bazdmeg1990', {
+    dialect: 'mysql',
+    host: 'localhost'
 });
 
-module.exports= pool;
+module.exports= sequelize;
