0cf3080cfa7dcf0f1e68586827eee05b364c5ea2
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-08 07:57:46 +0200

    add associations

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin')
-const shopRoutes = require('./routes/shop')
-const errorController =  require('./controllers/error')
-const sequelize  = require('./utils/database');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+const sequelize = require('./utils/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.set('view engine', 'ejs'); 
+app.set('view engine', 'ejs');
 app.set('views', 'views'); // set the views directory
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,9 +22,15 @@ app.use('/admin', adminRoutes);
 app.use('/', shopRoutes);
 app.use('/', errorController.pageNotFound);
 
-sequelize.sync().then((result) => {
-    console.log('Database synced successfully');
-    app.listen(PORT, () => console.log('Server is runnint at port ', + PORT));
-}).catch((err) => {
-    console.log('Error while syncing database : ', err);
-});
+
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+    .sync({ force: true })
+    .then((result) => {
+        console.log('Database synced successfully');
+        app.listen(PORT, () => console.log('Server is runnint at port ', + PORT));
+    }).catch((err) => {
+        console.log('Error while syncing database : ', err);
+    });
