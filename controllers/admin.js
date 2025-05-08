feecdf58210bc39b64d9d54ed95ffdf5d8699ca2
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-08 07:31:33 +0200

    update admin js with sequelize

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,17 @@
 const Product = require('../models/product');
 
 exports.getAdminProducts = (req, res, next) => {
-    Product.fetchAll()
-    .then(([rows, _filedData]) => {
-        res.render('admin/products', {
-            pageTitle: 'Admin Products Page',
-            prods: rows,
-            path: '/admin/products',
-        });
-    })
-    .catch(err => {
-        console.log('Error while fetching products : ', err)
-    });;
+    Product.findAll()
+        .then((rows) => {
+            res.render('admin/products', {
+                pageTitle: 'Admin Products Page',
+                prods: rows,
+                path: '/admin/products',
+            });
+        })
+        .catch(err => {
+            console.log('Error while fetching products : ', err);
+        });;
 };
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product', {
@@ -24,52 +24,46 @@ exports.getEditProduct = (req, res, next) => {
     const productId = req.params.productId;
     const editMode = req.query.edit;
 
-    Product.findById(productId, (product) => {
-        res.render('admin/edit-product', {
-            pageTitle: 'Edit Product',
-            path: '/admin/edit-product',
-            editing: editMode,
-            product: product,
+    Product.findByPk(productId)
+        .then((product) => {
+            res.render('admin/edit-product', {
+                pageTitle: 'Edit Product',
+                path: '/admin/edit-product',
+                editing: editMode,
+                product: product,
+            });
         });
-    });
 };
 exports.postEditProduct = (req, res, next) => {
-
-    const product = new Product({
+    const product = {
         title: req.body.title,
         price: req.body.price,
         description: req.body.description,
         imageUrl: req.body.imageUrl,
-        id: req.body.productId
-    });
-    console.log(product)
-    product.save()
-    .then((res) => {
-        console.log('insert response : ', res);
-        res.redirect('/');
-    })
+    };
+
+    Product
+        .update(product, { where: { id: req.body.productId } })
+        .then(_response => {
+            res.redirect('/');
+        });
 };
 exports.postDeleteProduct = (req, res, next) => {
-   
-    Product.delete(req.body.productId)
-    .then((deleteResponse) => {
-        console.log('delete response : ', deleteResponse);
-        res.redirect('/');
-    })
+    Product.destroy({ where: { id: req.body.productId } });
+    res.redirect('/');
 };
 
 exports.postAddProduct = (req, res, next) => {
-
     const product = {
         title: req.body.title,
         price: req.body.price,
         description: req.body.description,
         imageUrl: req.body.imageUrl
-    }
+    };
 
     Product.create(product)
-    .then((insertResponse) => {
-        console.log('insert response : ', insertResponse);
-        res.redirect('/');
-    })
+        .then((insertResponse) => {
+            // console.log('insert response : ', insertResponse);
+            res.redirect('/');
+        });
 };
