4fe95ab19840b6ad6d0baf7480ba7f699925cba7
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-12 08:30:00 +0200

    connect to mongodb

--- utils/database.js
+++ utils/database.js
@@ -1,9 +1,25 @@
-const Sequelize = require('sequelize')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-
-const sequelize = new Sequelize('node-server', 'root', 'Bazdmeg1990', {
-    dialect: 'mysql',
-    host: 'localhost'
+const uri = "mongodb+srv://hanta911:eGDTgOhiaQU1Wm36@node-cluster.lsrn2ml.mongodb.net/?retryWrites=true&w=majority&appName=Node-Cluster";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-module.exports= sequelize;
+const mongoConnect = (callBack) => {
+    client
+    .connect()
+    .then(client => {
+        callBack(client)
+    })
+    .catch(err => console.log(err))
+}
+
+
+
+
+module.exports= mongoConnect;
