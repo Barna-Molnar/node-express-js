a00f0683913870f020ac73b23b847de04b358c5c
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-14 08:52:03 +0200

    working on post cart fn

--- models/user.js
+++ models/user.js
@@ -1,31 +1,52 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../utils/database')
+const { ObjectId } = require('mongodb');
 
-/**
- * @typedef {Object} UserAttributes
- * @property {number} id
- * @property {string} name
- * @property {string} email
- */
+const getDb = require('../utils/database').getDb;
+class User {
+    constructor({ username, email, cart, id }) {
+        this.username = username;
+        this.email = email;
+        this.cart = cart || { items: [] }; // { items: [{...}, {...}]}
+        this._id = id;
+    }
 
-/**
- * @type {import('sequelize').ModelStatic<import('sequelize').Model<UserAttributes>>}
- */
-const User = sequelize.define('user', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-});
+    save() {
+        const db = getDb();
+        return db.collection('users')
+            .insertOne(this)
+            .then(result => {
+                console.log('INSERT RESULT FROM SAVING A USER', result);
+            })
+            .catch(err => console.log(err));
+    }
+
+    addToCart(productId) {
+        const updatedCartItems = [...this.cart.items];
+        
+        const searchedItemIndex = this.cart.items.findIndex(item => item.productId === productId);
+        if (searchedItemIndex >= 0) {
+            updatedCartItems[searchedItemIndex].quantity += 1;
+        } else {
+            updatedCartItems.push({ productId: productId, quantity: 1 });
+        }
+
+        const updatedCart = { items: updatedCartItems };
+        const db = getDb();
+        
+        return db.collection('users').updateOne({ _id: new ObjectId(this._id) }, { $set: { cart: updatedCart } });
+    }
+
+    static findById(userId) {
+        const db = getDb();
+        return db.collection('users')
+            .find({ _id: new ObjectId(userId) })
+            .next()
+            .then((user) => {
+                return user;
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+}
 
 module.exports = User;
--- routes/shop.js
+++ routes/shop.js
@@ -8,14 +8,14 @@ router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
 router.get('/products/:productId', shopController.getProduct);
 
-router.get('/cart', shopController.getCart);
+// router.get('/cart', shopController.getCart);
 router.post('/cart', shopController.postCart);
 
-router.post('/cart-delete-item', shopController.postDeleteProductFromCart);
+// router.post('/cart-delete-item', shopController.postDeleteProductFromCart);
 
-router.post('/create-order', shopController.postCreateOrder);
+// router.post('/create-order', shopController.postCreateOrder);
 
-router.get('/checkout', shopController.getCheckout);
-router.get('/orders', shopController.getOrders);
+// router.get('/checkout', shopController.getCheckout);
+// router.get('/orders', shopController.getOrders);
 
 module.exports = router;
