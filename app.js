44a61195c448c51073d55e45a08e3bff8a529bb9
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-14 08:50:04 +0200

    add user to req

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const connectToMongo = require('./utils/database').mongoConnect
+const connectToMongo = require('./utils/database').mongoConnect;
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const PORT = process.env.PORT || 8080;
 const app = express();
@@ -18,13 +19,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); // serve static files
 
 app.use((req, res, next) => {
-    // User.findByPk(1)
-    //     .then(user => {
-    //         req.user = user; // store user in all incoming request
-    //         next(); // go on with the next step
-    //     })
-    //     .catch(err => console.log(err));
-    next();
+    User.findById('682422f1ba6533b6dfe778cc')
+        .then(user => {
+            console.log('user from request',JSON.stringify(user))
+            req.user = new User({username: user.username, email: user.email, cart: user.cart, id: user._id }); // store user in all incoming request
+            next(); // go on with the next step
+        })
+        .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -35,4 +36,4 @@ app.use('/', errorController.pageNotFound);
 connectToMongo(() => {
     console.log('Connected to client... ');
     app.listen(PORT, () => console.log('Server is runnint at port ', + PORT));
-})
+});
