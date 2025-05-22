62735a3f614e5ff61ff137ed0d319ff473ce7834
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-05-22 07:53:49 +0200

    fix model

--- models/order.js
+++ models/order.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
     products: [
         {
-            productId: { type: Schema.Types.ObjectId, ref: 'Product', reqired: true },
+            product: { type: Object, reqired: true },
             quantity: { type: Number, reqired: true }
         }
     ],
