9c1c5d5b2a39aee2ad1ad6616fa7164a0c6dd178
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-06-05 07:02:26 +0200

    update project

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,25 @@
 const User = require('../models/user');
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        pageTitle: 'Signup',
+        path: '/login',
+        isLoggedIn: false
+    });
+};
+
+exports.postSignup = (req, res, next) => {
+    // User.findById('682ad496608187c07bf3ca42')
+    //     .then(user => {
+    //         console.log('postLogin....');
+    //         console.log({ user });
+    //         req.session.isLoggedIn = true;
+    //         req.session.user = user;
+    //         res.redirect('/');
+    //     })
+    //     .catch(err => console.log(err));
+};
+
 exports.getLogin = (req, res, next) => {
     res.render('auth/login', {
         pageTitle: 'Login',
