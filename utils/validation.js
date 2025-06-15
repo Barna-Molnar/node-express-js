433cd376083acd81bcf386cafd65d328ff5cede4
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-06-15 09:25:58 +0200

    develop validations

--- utils/validation.js
+++ utils/validation.js
@@ -1,20 +1,25 @@
-const User = require('../models/user')
 const { check, body } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
-const passwordValidation = () => [body('password').isLength({ min: 5 }).withMessage('Password is not long enough!')];
-const emailValidation = () => [
+const User = require('../models/user');
+
+const passwordValidation_signup = () => [
+    body('password')
+        .isLength({ min: 5 })
+        .withMessage('Password is not long enough!')
+];
+
+const emailValidation_signup = () => [
     check('email')
         .custom(async (value, { req }) => {
             const exisingUser = await User.findOne({ email: value });
             if (exisingUser) {
-                // req.flash('error', 'User already exist..., please choose another email!');
-                // await req.session.save();
-                // return res.redirect('/signup');
                 throw new Error('User already exist..., please choose another email!');
             }
         })
 ];
-const confirmedPasswordValidation = () => [
+
+const confirmedPasswordValidation_singup = () => [
     body('password').custom((value, { req }) => {
         if (value !== req.body.password) {
             throw new Error('Passwords have to match!');
@@ -22,4 +27,28 @@ const confirmedPasswordValidation = () => [
         return true;
     })];
 
-module.exports = {passwordValidation, emailValidation, confirmedPasswordValidation}
+const userValidation_login = () => [
+    body('email').notEmpty().withMessage('Email is required.'),
+    body('password').notEmpty().withMessage('Password is required.'),
+    
+    body()
+        .custom(async ({ email, password }, _) => {
+            const exisingUser = await User.findOne({ email: email });
+            if (!exisingUser) {
+                throw new Error('User not found! Please check your email!');
+            } else {
+                const isPasswordValid = await bcrypt.compare(password, exisingUser.password);
+                if (!isPasswordValid) {
+                    throw new Error('Invalid password...');
+                }
+            }
+            return true;
+        })
+];
+
+const validation = {
+    signup: { passwordValidation_signup, emailValidation_signup, confirmedPasswordValidation_singup },
+    login: { userValidation_login, }
+};
+
+module.exports = validation;
