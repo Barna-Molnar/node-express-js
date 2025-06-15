b9f830daddd21a01e3c70e8a64a1fa808869e00a
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-06-15 09:25:11 +0200

    add validation middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const { emailValidation, passwordValidation, confirmedPasswordValidation } = require('../utils/validation');
 
-const User = require('../models/user');
+const validations = require('../utils/validation');
+const { signup, login } = validations;
+
 const authController = require('../controllers/auth');
 const router = express.Router();
 
 router.get('/login', authController.getLogin);
-router.post('/login', authController.postLogin);
+router.post('/login', login.userValidation_login(), authController.postLogin);
 
 router.get('/signup', authController.getSignup);
-router.post('/signup', emailValidation(), passwordValidation(), confirmedPasswordValidation(), authController.postSignup);
+router.post('/signup', signup.emailValidation_signup(), signup.passwordValidation_signup(), signup.confirmedPasswordValidation_singup(), authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
